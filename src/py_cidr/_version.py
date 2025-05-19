86038d058e82a208ea47a791f68bfae0cfe54f73
Gene C <arch@sapience.com>
2025-05-19 05:25:22 -0400

    Add pytest to checkdepnds in arch PKBUILD   Thanks to @Reylak pm AUR.

--- src/py_cidr/_version.py
+++ src/py_cidr/_version.py
@@ -4,8 +4,8 @@
 Project py-cidr
 This file is auto updated by git-release
 """
-__version__ = "3.2.1"
-__date__ = "2025-05-04"
+__version__ = "3.2.2"
+__date__ = "2025-05-19"
 __reldev__ = "release"
 
 
